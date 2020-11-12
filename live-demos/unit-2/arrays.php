51421287f37b136fac85829498499da867069aea
Elly Post <elly@ellytronic.media>
2020-11-11 20:50:55 -0500

    Add arrays demo and add a couple quick fixes to lab files before lab starts

---
+++ live-demos/unit-2/arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+// indexed array (like a list in python)
+$my_array = array();
+$my_array[0] = "Value 1";
+$my_array[1] = "Value 2";
+$my_array[4] = "Value 3";
+// our array looks like [value 1, value 2, null, null, value 3]
+
+// associative array (like a dictionary in python)
+// also called a map (also hashtable, etc)
+$my_array = array();
+$my_array = [];
+$my_array["maniar"] = 5555555555;
+$my_array["post"] = 5558479595;
+$my_array[] = 123456789;
+$my_array[0] = 987645321;
+
